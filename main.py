16e1c7b3c53bea6f690c5c8604c9d51f02c34de8
Sylvestre Gug <sylvestre@observablehq.com>
2023-06-04 11:44:06 -0400

    WIP

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import jwt
 import os
+import pinecone
 
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
@@ -13,7 +14,13 @@ JWT_EXPIRATION_TIME_MINUTES = 30
 security = HTTPBearer()
 
 
-@app.get("/query")
+@app.get("/_status")
+def read_root():
+    return {"status": "ok"}
+
+
+# AUTH ROUTES
+@app.post("/query")
 async def protected_endpoint(credentials: HTTPAuthorizationCredentials = Depends(security), query: str = None):
     try:
         payload = jwt.decode(credentials.credentials, JWT_SECRET, algorithms=[JWT_ALGORITHM])
@@ -23,6 +30,22 @@ async def protected_endpoint(credentials: HTTPAuthorizationCredentials = Depends
     except jwt.DecodeError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
-    # Endpoint logic goes here
+    pinecone.init(api_key="YOUR_API_KEY", environment="YOUR_ENVIRONMENT")
+    index = pinecone.Index("example-index")
+
+    query_response = index.query(
+        namespace="example-namespace",
+        top_k=10,
+        include_values=True,
+        include_metadata=True,
+        vector=[0.1, 0.2, 0.3, 0.4],
+        sparse_vector={
+            'indices': [10, 45, 16],
+            'values': [0.5, 0.5, 0.2]
+        },
+        filter={
+            "genre": {"$in": ["comedy", "documentary", "drama"]}
+        }
+    )
 
     return {"message": f"Protected endpoint accessed successfully by {username}"}
